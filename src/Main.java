f524dd3acfa218a63fd36399f7606670efbcfd0c
Fyodor <samoilovfs@gmail.com>
2024-10-05 12:01:30 +0400

    исправить замечания ревьюера

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
+import java.util.Arrays;
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         int sizeArr = 8;
@@ -9,7 +9,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
 
-        while (true){
+        while (true) {
             System.out.println("Выберите одну из команд:");
             System.out.println("1. Добавить товар в список");
             System.out.println("2. Показать список");
@@ -18,53 +18,46 @@ public class Main {
 
             int actionNumber = scanner.nextInt();
 
-            switch (actionNumber){
+            switch (actionNumber) {
                 case 1:
                     String newProduct;
-                    for (int i = 0; i < sizeArr; i++) {
-                        if (productCount >= sizeArr) {
-                            sizeArr++;
-                            String[] tempArr = new String[sizeArr];
-                            System.arraycopy(shoppingList, 0, tempArr, 0, sizeArr - 1);
-                            shoppingList = tempArr;
-                        }
-                        if (shoppingList[i] == null){
-                            System.out.println("Введите название нового товара");
-                            scanner.nextLine();
-                            newProduct = scanner.nextLine().replace(" ", "_");
-                            boolean serchForDuplicates = true;
-                            for (int j = 0; j < sizeArr; j++) {
-                                if (newProduct.equals(shoppingList[j])) {
-                                    serchForDuplicates = false;
-                                }
-                            }
-                            if (serchForDuplicates) {
-                                shoppingList[i] = newProduct;
-                                System.out.println("Товар " + newProduct + " успешно добавлен под номером " + (i + 1));
-                                productCount++;
-                                break;
-                            } else {
-                                System.out.println("Такой товар уже существует");
-                            }
+                    if (productCount >= sizeArr) {
+                        sizeArr++;
+                        String[] tempArr = new String[sizeArr];
+                        System.arraycopy(shoppingList, 0, tempArr, 0, sizeArr - 1);
+                        shoppingList = tempArr;
+                    }
+
+                    System.out.println("Введите название нового товара");
+                    scanner.nextLine();
+                    newProduct = scanner.nextLine().replace(" ", "_");
+                    boolean serchForDuplicates = true;
+                    for (int j = 0; j < sizeArr; j++) {
+                        if (newProduct.equalsIgnoreCase(shoppingList[j])) {
+                            serchForDuplicates = false;
                             break;
                         }
                     }
+                    if (serchForDuplicates) {
+                        shoppingList[productCount] = newProduct;
+                        System.out.println("Товар " + newProduct + " успешно добавлен под номером " + (productCount + 1));
+                        productCount++;
+                        break;
+                    } else {
+                        System.out.println("Такой товар уже существует");
+                    }
                     break;
                 case 2:
-                    if (productCount > 0){
+                    if (productCount > 0) {
                         for (int i = 0; i < productCount; i++) {
-                            if (shoppingList[i] != null)
-                                System.out.println(i+1 + "." + shoppingList[i]);
+                            System.out.println(i + 1 + "." + shoppingList[i]);
                         }
-                    }else {
+                    } else {
                         System.out.println("Список покупок пуст");
                     }
-
                     break;
                 case 3:
-                    for (int i = 0; i < productCount; i++) {
-                        shoppingList[i] = null;
-                    }
+                    Arrays.fill(shoppingList, null);
                     productCount = 0;
                     System.out.println("Список покупок пуст");
                     break;
